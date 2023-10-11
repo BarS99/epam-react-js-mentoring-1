25744d6754539a7a36ae63522dc8867620539648
Bartlomiej Swiech <bartlomiej_swiech@epam.com>
2023-10-11 22:03:51 +0200

    feat: implementation of module 6 - routing

---
+++ src/router/pages/MovieListPage/components/MovieListPageHeader.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from "react";
+import styles from "./MovieListPageHeader.module.scss";
+import Logo from "../../../../components/Logo";
+
+const MovieListPageHeader = ({ children }: { children?: ReactNode }) => (
+	<header className={styles["movie-list-page-header"]}>
+		<Logo />
+		{children}
+	</header>
+);
+
+export default MovieListPageHeader;
